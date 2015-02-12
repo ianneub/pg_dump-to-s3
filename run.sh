d4e9a5480df9786591a9784dd1eb8d6aee7ab11e
Ian Neubert <ian@ianneubert.com>
2015-02-11 17:42:32 -0800

    Use the correct Postgres port and pass env vars.

--- run.sh
+++ run.sh
@@ -32,26 +32,26 @@ if [ -z "${POSTGRES_ENV_POSTGRES_USER}" ]; then
   exit 1
 fi
 
-if [ -z "${POSTGRES_ENV_POSTGRES_PASS}" ]; then
+if [ -z "${POSTGRES_ENV_POSTGRES_PASSWORD}" ]; then
   echo "You need to set the POSTGRES_ENV_POSTGRES_PASS environment variable."
   exit 1
 fi
 
-if [ -z "${POSTGRES_PORT_3306_TCP_ADDR}" ]; then
-  echo "You need to set the POSTGRES_PORT_3306_TCP_ADDR environment variable or link to a container named MYSQL."
+if [ -z "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
+  echo "You need to set the POSTGRES_PORT_5432_TCP_ADDR environment variable or link to a container named MYSQL."
   exit 1
 fi
 
-if [ -z "${POSTGRES_PORT_3306_TCP_PORT}" ]; then
-  echo "You need to set the POSTGRES_PORT_3306_TCP_PORT environment variable or link to a container named MYSQL."
+if [ -z "${POSTGRES_PORT_5432_TCP_PORT}" ]; then
+  echo "You need to set the POSTGRES_PORT_5432_TCP_PORT environment variable or link to a container named MYSQL."
   exit 1
 fi
 
-POSTGRES_HOST_OPTS="-h $POSTGRES_PORT_3306_TCP_ADDR -p $POSTGRES_PORT_3306_TCP_PORT -U $POSTGRES_ENV_POSTGRES_USER"
+POSTGRES_HOST_OPTS="-h $POSTGRES_PORT_5432_TCP_ADDR -p $POSTGRES_PORT_5432_TCP_PORT -U $POSTGRES_ENV_POSTGRES_USER"
 
-echo "Starting dump of ${PGDUMP_DATABASE} database(s) from ${POSTGRES_PORT_3306_TCP_ADDR}..."
+echo "Starting dump of ${PGDUMP_DATABASE} database(s) from ${POSTGRES_PORT_5432_TCP_ADDR}..."
 
-export PGPASSWORD=${POSTGRES_ENV_POSTGRES_PASS}
+export PGPASSWORD=${POSTGRES_ENV_POSTGRES_PASSWORD}
 
 pg_dump $PGDUMP_OPTIONS $POSTGRES_HOST_OPTS $PGDUMP_DATABASE | aws s3 cp - s3://$AWS_BUCKET/$PREFIX/$(date +"%Y")/$(date +"%m")/$(date +"%d").dump || exit 2
 
